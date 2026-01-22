9a846f27da12b35c5e35c6210d283c19a900eb1b
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-21 19:52:15 -0800

    v1 final

--- main.py
+++ main.py
@@ -6,13 +6,32 @@ import random
 colors = [(221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221), (57, 51, 48), (184, 103, 113), (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210), (65, 66, 56)]
 
 screen = t.Screen()
-screen.setup(600, 600)
+screen.setup(800, 800)
 
 timmy = t.Turtle()
 
 t.colormode(255)
+timmy.speed("fastest")
 
-timmy.pencolor(random.choice(colors))
+timmy.penup()
+timmy.hideturtle()
 
+if __name__ == "__main__":
+    timmy.setheading(224)
+    timmy.forward(500)
+    timmy.setheading(0)
+    number_of_dots = 225
 
-t.mainloop()
+    for dot_count in range(1, number_of_dots+1):
+        timmy.dot(20, random.choice(colors))
+        timmy.forward(50)
+
+        if dot_count % 15 == 0:
+            timmy.setheading(90)
+            timmy.forward(50)
+            timmy.setheading(180)
+            timmy.forward(750)
+            timmy.setheading(0)
+    screen.save("my_drawing.ps", overwrite=True)
+
+    t.mainloop()
